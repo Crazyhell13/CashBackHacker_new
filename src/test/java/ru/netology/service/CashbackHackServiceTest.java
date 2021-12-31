4596255c68b956d1943a8b2c286e455c27caef9c
crazyhell13 <crazyhell13@gmail.com>
2021-12-31 15:43:17 +0300

    Initial commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,27 +1,10 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
-    @Test
-    public void shouldReturn100IfAmountIs900() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 900;
-        int actual = service.remain(amount);
-        int expected = 100;
-        assertEquals(actual, expected);
-    }
-
-    @Test
-    public void shouldReturn800IfAmountIs1200() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1200;
-        int actual = service.remain(amount);
-        int expected = 800;
-        assertEquals(actual, expected);
-    }
 
     @Test
     public void shouldReturn0IfAmountIs1000() {
@@ -29,7 +12,24 @@ public class CashbackHackServiceTest {
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
+    @Test
+    public void shouldReturn100IfAmountIs900() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturn200IfAmountIs800() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 800;
+        int actual = service.remain(amount);
+        int expected = 200;
+        assertEquals(expected, actual);
+    }
 }
